b5ac00f13d3517f3d47222be8e935708a4a75fa9
raghavendra <raghureddy9398@gmaul.com>
2023-11-02 13:16:52 +0530

    kanna

---
+++ age.java
@@ -0,0 +1,15 @@
+mclass Age
+{
+	public static void main(String[] args)
+	{
+	int age=25;
+	if(age>=18)
+         {
+	 System.out.println("you are elligible");
+	 }
+	 else
+	 {
+          System.out.println("you are not elligible");
+         }
+        }
+}
